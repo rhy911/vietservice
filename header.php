adc20d18dff39e80e82b6b1954095b47176c8785
rhy911 <minhcute911@gmail.com>
2024-10-17 15:55:57 +0700

    🎉 init:

---
+++ header.php
@@ -0,0 +1,13 @@
+<header class="header">
+    <img src="assets/images/logo.png" alt="Viet Service Logo" class="logo">
+    <div class="nav-bar">
+        <nav class="nav">
+            <a href="#">TRANG CHỦ</a>
+            <a href="#">GIỚI THIỆU</a>
+            <a href="#">TIN TỨC</a>
+            <a href="#">DỊCH VỤ</a>
+            <a href="#">LIÊN HỆ</a>
+        </nav>
+    </div>
+    <link rel="stylesheet" href="styles/header.css?v=<?php echo time(); ?>" />
+</header>
