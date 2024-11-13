793bc14e336f5694f251c28ec9ca848012fca99f
rhy911 <minhcute911@gmail.com>
2024-11-13 21:37:27 +0700

    ✨ feat:

--- aboutus.php
+++ aboutus.php
@@ -20,13 +20,13 @@
             </div>
         </section>
         <section style="padding: 60px 20px 60px 20px;">
-            <div class="max-width d-flex">
-                <div class="d-flex flex-column justify-content-center width-50 padding-10">
+            <div class="about-us-text-content-1 max-width d-flex">
+                <div class="about-us-text-content-1-element">
                     <h2>DỊCH VỤ IT SERVICES ĐÁNG TIN CẬY</h2>
                     <div>
                         <div class="stats d-flex">
                             <div class="container">
-                                <div class="row justify-content-center text-start">
+                                <div class="row text-start">
                                     <div class="col-md-4">
                                         <span id="count1" class="display-4"></span>
                                         <span class="display-4 light">+</span>
@@ -55,7 +55,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="width-50 padding-10">
+                <div class="about-us-text-content-1-element">
                     <p>Doanh nghiệp luôn có những nhu cầu về lưu trữ và quản lý các dữ liệu quan trọng, và cần đảm bảo dữ liệu được lưu trữ trong một môi trường an toàn và đảm bảo rằng nó có thể dễ dàng truy cập được. Nhưng có những quy định chỉ những người đặc biệt được phép xem.</p>
                     <p>Dựa theo nhu cầu này, nếu không có sự hỗ trợ của nhóm CNTT chuyên nghiệp, doanh nghiệp có thể dễ dàng lưu trữ dữ liệu ở vị trí dễ bị tấn công hoặc thậm chí cấp quyền truy cập cho ai đó mà không có quyền phù hợp, đây có thể là một vấn đề lớn cần quan tâm đến việc bảo vệ dữ liệu. </p>
                     <p>Chúng tôi có sứ mệnh là cung cấp các dịch vụ Services tối ưu nhất, tiết kiệm nhất cho các doanh nghiệp, tạo tiền đề cho các doanh nghiệp này tập trung vào nâng cao năng suất, từ đó kéo theo sự phát triển nền kinh tế nước nhà.</p>
@@ -64,7 +64,7 @@
         </section>
         <section>
             <div class="devider"></div>
-            <div class="max-width d-flex">
+            <div class="about-us-text-content-2 max-width d-flex">
                 <div class="card">
                     <div class="img-box"><img src="assets/images/Tamnhin.png" alt="aboutus" class="img-fluid"></div>
                     <div>
@@ -198,7 +198,9 @@
     </div>
     <?php include 'shared/footer.php'; ?>
     <div id="video-container">
-        <iframe src="https://www.youtube.com/embed/XHOmBV4js_E?si=eftZaf3DPLpZf3TL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div class="video-wrapper">
+            <iframe src="https://www.youtube.com/embed/XHOmBV4js_E?si=eftZaf3DPLpZf3TL?controls=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="scripts/number_counter.js"></script>
